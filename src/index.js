ced47961f34f73cec14841bea62c373e71157639
cbargan <utopiaeh01@gmail.com>
2019-11-04 17:24:47 +0200

    Added absolute imports and eslint import orders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom'
 
 import { Provider } from 'react-redux'
-import store from './redux/redux-store'
-
-import './index.css'
 import App from './App'
+import store from './redux/redux-store'
+import './index.css'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
