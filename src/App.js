1432e40265a7246e4983af541fb1a683911a5b21
cbargan <utopiaeh01@gmail.com>
2019-11-06 00:00:05 +0200

    Ui-kit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>{`Edit <code>src/App.js</code> and save to reload.`}</p>
         <p> Hello </p>
-        <Button title="test" />
+        <Button />
       </header>
     </div>
   )
--- src/ui-kit/button.js
+++ src/ui-kit/button.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { string } from 'prop-types'
 import { Button as AntdButton } from 'antd'
 
+const DEFAULT_TITLE = 'Submit'
 const Button = ({ title }) => (
   <span>
-    <AntdButton style={{ fontSize: '25px' }}>{title}</AntdButton>
+    <AntdButton style={{ fontSize: '15px' }}>{title}</AntdButton>
   </span>
 )
 Button.propTypes = {
-  title: PropTypes.func.isRequired,
+  title: string,
+}
+Button.defaultProps = {
+  title: DEFAULT_TITLE,
 }
 export default Button
