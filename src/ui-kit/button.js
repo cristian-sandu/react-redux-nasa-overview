6b4c1372ca8289a715cd35c3e7c3ddb738f7f459
cbargan <utopiaeh01@gmail.com>
2019-11-05 23:04:58 +0200

    Ui-kit

--- src/ui-kit/button.js
+++ src/ui-kit/button.js
@@ -1,11 +1,13 @@
-/* eslint-disable */
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Button as AntdButton } from 'antd'
 
 const Button = ({ title }) => (
   <span>
-    <AntdButton style={{ fontSize: '25px' }}> {title} </AntdButton>
+    <AntdButton style={{ fontSize: '25px' }}>{title}</AntdButton>
   </span>
 )
-
+Button.propTypes = {
+  title: PropTypes.func.isRequired,
+}
 export default Button
--- src/ui-kit/index.js
+++ src/ui-kit/index.js
@@ -1,2 +1,3 @@
-/* eslint-disable */
 export { default as Button } from './button'
+
+export default null
