f5e0d868f661c981283f76ce3115c19092cc9a21
csandu <sandu.cristian@mail.ru>
2019-11-05 12:54:29 +0200

    Add Absolute Imports support > groups

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import './App.css'
 
-import { EMPTY_STRING } from 'common/constants' //eslint-disable-line
-import { UTIL } from 'utils' //eslint-disable-line
+import { EMPTY_STRING } from 'common/constants'
+import { UTIL } from 'utils'
+
 import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{`Edit <code>src/App.js</code> and save to reload.`}</p>
+        <p>{`Edit <code>src/App.js</code> and save to reload.${UTIL} ${EMPTY_STRING}`}</p>
         <p> Hello </p>
         <a
           className="App-link"
