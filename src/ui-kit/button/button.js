8011fee195b0949ebc13ec8f029836ff3ea175d8
cbargan <utopiaeh01@gmail.com>
2019-11-06 17:46:00 +0200

    Ui-kit and add scss directory in ui-kit

--- src/ui-kit/button/button.js
+++ src/ui-kit/button/button.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { string } from 'prop-types'
 import { Button as ButtonAntD } from 'antd'
-import './scss/button.scss'
+
+import './button.scss'
 
 const DEFAULT_TITLE = 'Submit'
+
 const Button = ({ title }) => (
-  <span>
-    <ButtonAntD className="test">{title}</ButtonAntD>
+  <span className="ui-kit__button-container">
+    <ButtonAntD className="ui-kit__button">{title}</ButtonAntD>
   </span>
 )
 
